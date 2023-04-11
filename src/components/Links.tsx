35f01a240d9deb8f6aef415c88170d64db2d2e27
Jairotsb <jairo_tunisse@outlook.com>
2023-04-10 23:45:06 -0300

    feat: structure project updated :white_check_mark:

---
+++ src/components/Links.tsx
@@ -0,0 +1,13 @@
+export default function Links(){
+
+    return(
+        <ul>
+            <li>Links</li>
+            <li>Links</li>
+            <li>Links</li>
+            <li>Links</li>
+        </ul>
+
+    );
+
+}
